0b76e2e92a6a16e52095ade70a3f212b4645bae7
mdamirulislam313 <mdmamirllmama@gmail.com>
2025-09-16 14:26:14 +0600

    adedd navdar section

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import DaisyNav from "./components/Daisynav/DaisyNav";
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
+     
+    </>
+  );
+}
+
+export default App;
